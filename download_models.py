b3f593da3a1e922e7caa5eae90d3011899a28804
moaazreda55 <111019977+moaazreda55@users.noreply.github.com>
2025-06-29 23:11:49 +0300

    Update download_models.py

--- download_models.py
+++ download_models.py
@@ -12,7 +12,3 @@ def download_model(file_id,name):
 
 download_model("1v_e7Pssu9QLjk9M4VwampuXK5_0XZKSh","shelv.pt")
 download_model("1sNVJkvPmDYym_C7WsBkfc-n04LxdtmnQ","product.pt")
-# "12fv8hk5VjPAN5vjVkpgavC2PemekPq"
-# "https://drive.google.com/file/d/1sNVJkvPmDYym_C7WsBkfc-n04LxdtmnQ"
-# https://drive.google.com/file/d/1v_e7Pssu9QLjk9M4VwampuXK5_0XZKSh/view?usp=sharing
-# https://drive.google.com/file/d/1sNVJkvPmDYym_C7WsBkfc-n04LxdtmnQ/view?usp=sharing
